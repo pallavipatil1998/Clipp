064974f441231e03cc7a1564a9117e40ea20470a
Pallavi Patil <pallavishravan545@gmail.com>
2025-04-04 11:33:14 +0530

    BackDrop Filtter

---
+++ android/app/src/main/kotlin/com/example/clipp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clipp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Clipp()
+    );
+  }
+}
+
+
+class Clipp extends StatelessWidget {
+  const Clipp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: SizedBox( height: 500,
+                width: 500,
+                child: Image.asset("assets/images/img_natural9.jpg")),
+          ),
+          BackdropFilter(filter: ImageFilter.blur(
+            sigmaX: 10.0,sigmaY: 10.0,
+
+          ),
+            child:Container(child: Center(
+              child: Text("It\s Raining",style: TextStyle(fontSize: 50,color: Colors.white,fontWeight: FontWeight.bold),
+
+              ),
+            ),) ,
+          ),
+
+        ],
+      ),
+    );
+  }
+}
